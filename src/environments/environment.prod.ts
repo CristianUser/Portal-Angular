e28440a0692f697e7977ff285781676af9ea1a34
CristianUser <theimaginario@hotmail.com>
2018-10-30 22:43:46 -0400

    update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDVR0igmIWj0GXjuL4kHxCbVoLirzrE65s",
+    authDomain: "portal-app-6e65b.firebaseapp.com",
+    databaseURL: "https://portal-app-6e65b.firebaseio.com",
+    projectId: "portal-app-6e65b",
+    storageBucket: "portal-app-6e65b.appspot.com",
+    messagingSenderId: "796463783217"
+  }
 };
